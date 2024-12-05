22077b76fed30c19d1a499a139f6e6cda587dc23
Nathan Race <racecn@gmail.com>
2024-12-05 07:22:47 -0500

    refactor: simplify class name utility function

--- frontend/src/lib/utils.jsx
+++ frontend/src/lib/utils.jsx
@@ -1,6 +1,5 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+function cn(...classes) {
+  return classes.filter(Boolean).join(' ');
 }
+
+export { cn };
